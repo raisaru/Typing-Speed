247412a2440dc8cb8ba37d0950c478d3ef003f3f
Saru Rai <sarurai@parijat.asia>
2026-07-13 12:53:45 +0545

    Fix: apply relative asset pathing for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- 1. Import Tailwind
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // <-- ADD THIS LINE HERE TO FIX THE BLANK SCREEN
   plugins: [
     react(),
-    tailwindcss(), // <-- 2. Inject it into the plugins array
+    tailwindcss(),
   ],
 })
